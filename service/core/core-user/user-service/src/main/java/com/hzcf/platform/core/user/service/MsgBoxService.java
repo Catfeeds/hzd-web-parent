5f93ddf292ebc44e5b725cbbc14b1401c85d8cb0
leijiaming <13911890913@139.com>
2016-12-30 09:53:38 +0800

    修改msgboxservice

--- service/core/core-user/user-service/src/main/java/com/hzcf/platform/core/user/service/MsgBoxService.java
+++ service/core/core-user/user-service/src/main/java/com/hzcf/platform/core/user/service/MsgBoxService.java
@@ -1,25 +1,10 @@
 package com.hzcf.platform.core.user.service;
 
-import java.util.Map;
-
-import com.hzcf.platform.common.util.rpc.result.PaginatedResult;
 import com.hzcf.platform.core.user.model.MsgBoxVO;
 import com.hzcf.platform.framework.core.service.IBaseService;
 
 /**
- * 
- * @description:msgBox功能操作
- * @author 李强
- * @version 1.0
- * 
- * <pre>
- * Modification History: 
- * Date              Author      Version     Description 
- * ------------------------------------------------------------------ 
- * 2016年12月29日                             李强                             1.0         1.0 Version 
- * </pre>
+ * Created by leijiaming on 2016/12/30 0030.
  */
-public interface MsgBoxService  extends IBaseService<MsgBoxVO> {
-	
-	public PaginatedResult<MsgBoxVO> getMsgBoxPage(Map<String, Object> parmMap);
+public interface MsgBoxservice extends IBaseService<MsgBoxVO> {
 }
--- service/core/core-user/user-serviceimpl/src/main/java/com/hzcf/platform/core/user/service/impl/MsgBoxServiceImpl.java
+++ service/core/core-user/user-serviceimpl/src/main/java/com/hzcf/platform/core/user/service/impl/MsgBoxServiceImpl.java
@@ -1,27 +1,20 @@
 package com.hzcf.platform.core.user.service.impl;
 
-import com.hzcf.platform.common.util.rpc.result.PaginatedResult;
-import com.hzcf.platform.common.util.status.StatusCodes;
 import com.hzcf.platform.core.user.dao.MsgBoxDao;
 import com.hzcf.platform.core.user.data.MsgBox;
 import com.hzcf.platform.core.user.model.MsgBoxVO;
-import com.hzcf.platform.core.user.service.MsgBoxService;
+import com.hzcf.platform.core.user.service.MsgBoxservice;
 import com.hzcf.platform.framework.core.service.impl.AbstractBaseServiceImpl;
 import com.hzcf.platform.framework.core.storage.IBaseDao;
-
-import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 /**
- * Created by leijiaming on 2016/12/29 0029.
+ * Created by leijiaming on 2016/12/30 0030.
  */
-@Service
-public class MsgBoxServiceImpl   extends AbstractBaseServiceImpl<MsgBoxVO,MsgBox> implements MsgBoxService {
-    private Logger logger = LoggerFactory.getLogger(MsgBoxServiceImpl.class);
+public class MsgBoxserviceImpl  extends AbstractBaseServiceImpl<MsgBoxVO,MsgBox> implements MsgBoxservice {
+    private Logger logger = LoggerFactory.getLogger(MsgBoxserviceImpl.class);
 
     @Autowired
     private MsgBoxDao purchaseOrderDao;
@@ -41,28 +34,4 @@ public class MsgBoxServiceImpl   extends AbstractBaseServiceImpl<MsgBoxVO,MsgBox
         return purchaseOrderDao;
     }
 
-    /**
-     * 获取站内消息
-     */
-	@Override
-	public PaginatedResult<MsgBoxVO> getMsgBoxPage(Map<String, Object> parmMap) {
-		try {
-			PaginatedResult<MsgBox> result = purchaseOrderDao.getMsgBoxPage(parmMap);
-			if (null == result || result.getItems() == null) {
-				logger.debug("data null.");
-				PaginatedResult<MsgBoxVO> resultVO = new PaginatedResult<MsgBoxVO>();
-				resultVO.setStatus(StatusCodes.OK);
-				return resultVO;
-			}
-			PaginatedResult<MsgBoxVO> resultVO = new PaginatedResult<MsgBoxVO>();
-			resultVO.setItems(toVO(result.getItems()));
-			resultVO.setStatus(StatusCodes.OK);
-			return resultVO;
-		} catch (Exception e) {
-			logger.error("an error occur in getByPK service : {}", e);
-			PaginatedResult<MsgBoxVO> resultVO = new PaginatedResult<MsgBoxVO>();
-			resultVO.setStatus(StatusCodes.INTERNAL_SERVER_ERROR);
-			return resultVO;
-		}
-	}
 }
