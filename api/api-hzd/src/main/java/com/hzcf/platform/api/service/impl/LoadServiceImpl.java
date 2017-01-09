f7f04cd85d516d6cbffd3f7874616610997f8900
peigaoxiang <pgx19890112@163.com>
2017-01-09 15:57:52 +0800

    完善对接

--- api/api-hzd/src/main/java/com/hzcf/platform/api/service/impl/LoadServiceImpl.java
+++ api/api-hzd/src/main/java/com/hzcf/platform/api/service/impl/LoadServiceImpl.java
@@ -15,6 +15,8 @@ import com.hzcf.platform.common.util.log.Log;
 import com.hzcf.platform.core.user.model.UserVO;
 import com.hzcf.platform.webService.LoadService;
 
+import net.sf.json.JSONObject;
+
 /**
   * @Description:借款信息的service
   * @author 作者:裴高祥 
@@ -31,7 +33,7 @@ public class LoadServiceImpl implements ILoadService {
 	@Override
 	public BackResult insertLoad(UserVO user, Map map) {
 		String result=LoadService.insertLoad(user,map);
-		if (StringUtils.isNotBlank(result)) {
+		if (StringUtils.isNotBlank(result) && "0000".equals(JSONObject.fromObject(result).getString("retCode"))) {
 			logger.i("接口：进件成功，结果："+result);
 			return new BackResult(HzdStatusCodeEnum.MEF_CODE_0000.getCode(), HzdStatusCodeEnum.MEF_CODE_0000.getMsg());
 		}else{
@@ -45,7 +47,7 @@ public class LoadServiceImpl implements ILoadService {
 	@Override
 	public BackResult selectLoadProgress(UserVO user) {
 		String result=LoadService.selectLoadProgress(user);
-		if (StringUtils.isNotBlank(result)) {
+		if (StringUtils.isNotBlank(result) && "0000".equals(JSONObject.fromObject(result).getString("retCode"))) {
 			logger.i("接口：借款人查询借款进度成功，结果："+result);
 			return new BackResult(HzdStatusCodeEnum.MEF_CODE_0000.getCode(), HzdStatusCodeEnum.MEF_CODE_0000.getMsg());
 		}else{
