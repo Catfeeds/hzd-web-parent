a94dcb43e658502c0bb9a1f5f1aa46ba3c1f852b
zhangmingxing <zhangmingxing@huizhongcf.com>
2017-01-09 19:52:32 +0800

    字典数据转码

--- service/core/core-user/user-service/src/main/java/com/hzcf/platform/core/user/service/DictUtilService.java
+++ service/core/core-user/user-service/src/main/java/com/hzcf/platform/core/user/service/DictUtilService.java
@@ -25,4 +25,45 @@ public interface DictUtilService {
     public List<UserDictJson> applyDictionaryRegionsheng();
     public Map<String,Object> applyDictionaryRegionshi();
     public Map<String,Object> applyDictionaryRegionqu();
+    
+    
+    /**
+     * 借款用途 大类
+     * @param value
+     * @return
+     */
+    public String convertLoanPurposeOne(String value);
+    /**
+     * 借款用途 小类
+     * @param value
+     * @return
+     */
+    public String convertLoanPurposeTwo(String parentVale, String value);
+    /**
+     * 其他字典   - 房产状况等等
+     * @param type    类型  “HOUSE_STATUS/LIVE_TOGETHER/CHILDRE_STATUS” 
+     * @param value   值
+     * @return
+     */
+    public String convertDict(String type, String value) ;
+    /**
+     * 省份
+     * @param value
+     * @return
+     */
+    public String convertProvince(String value) ;
+    /**
+     * 市
+     * @param provinceValue 省份value
+     * @param value
+     * @return
+     */
+    public String convertCity(String provinceValue, String value) ;
+    /**
+     * 区、县
+     * @param cityValue 市value
+     * @param value
+     * @return
+     */
+    public String convertArea(String cityValue, String value) ;
 }
--- service/core/core-user/user-serviceimpl/src/main/java/com/hzcf/platform/core/user/commom/dictTools/DictUtilServiceImpl.java
+++ service/core/core-user/user-serviceimpl/src/main/java/com/hzcf/platform/core/user/commom/dictTools/DictUtilServiceImpl.java
@@ -39,4 +39,111 @@ public class DictUtilServiceImpl  implements DictUtilService {
     @Override
     public Map<String, Object> applyDictionaryRegionqu() {
         return (Map<String, Object>) cache.load("applyDictionaryRegionqu");    }
+    
+    
+    /**
+     * 借款用途 大类
+     * @param value
+     * @return
+     */
+    @Override
+    public String convertLoanPurposeOne(String value) {
+    	List<UserDictJson> result = (List<UserDictJson>) cache.load("applyDictionaryJkyt");
+    	for(UserDictJson dict : result){
+    		if(value.equals(dict.getDict_value())){
+    			return dict.getDict_text();
+    		}
+    	}
+    	return "";
+    }
+    
+    /**
+     * 借款用途 小类
+     * @param parentVale
+     * @param value
+     * @return
+     */
+    @Override
+    public String convertLoanPurposeTwo(String parentVale, String value) {
+    	List<UserDictJson> result = (List<UserDictJson>) cache.load("applyDictionaryJkyt");
+    	List<UserDictJson> tempList = null;
+    	for(UserDictJson dict : result){
+    		if(parentVale.equals(dict.getDict_value())){
+    			tempList = dict.getList();
+    			for(UserDictJson temp : tempList){
+    				if(value.equals(temp.getDict_value()))
+    				return temp.getDict_text();
+    			}
+    		}
+    	}
+    	return "";
+    }
+    
+    /**
+     * 其他字典   - 房产状况等等
+     * @param type    类型  “HOUSE_STATUS/LIVE_TOGETHER/CHILDRE_STATUS” 
+     * @param value   值
+     * @return
+     */
+    @Override
+    public String convertDict(String type, String value) {
+    	Map<String, Object> result = (Map<String, Object>) cache.load("applyDictionaryinfo");
+        List<UserDictJson> tempList = (List<UserDictJson>)result.get(type);
+        for(UserDictJson dict : tempList){
+    		if(value.equals(dict.getDict_value())){
+    			return dict.getDict_text();
+    		}
+    	}
+    	return "";
+    }
+    
+    /**
+     * 省份
+     * @param value
+     * @return
+     */
+    @Override
+    public String convertProvince(String value) {
+    	List<UserDictJson> result = (List<UserDictJson>) cache.load("applyDictionaryRegionsheng");
+    	for(UserDictJson dict : result){
+    		if(value.equals(dict.getDict_value())){
+    			return dict.getDict_text();
+    		}
+    	}
+    	return "";
+    }
+    
+    /**
+     * 市
+     * @param provinceValue 省份value
+     * @param value
+     * @return
+     */
+    public String convertCity(String provinceValue, String value) {
+    	Map<String, Object> result = (Map<String, Object>) cache.load("applyDictionaryRegionshi");
+        List<UserDictJson> tempList = (List<UserDictJson>)result.get(provinceValue);
+        for(UserDictJson dict : tempList){
+    		if(value.equals(dict.getDict_value())){
+    			return dict.getDict_text();
+    		}
+    	}
+    	return "";
+    }
+    
+    /**
+     * 区、县
+     * @param cityValue 市value
+     * @param value
+     * @return
+     */
+    public String convertArea(String cityValue, String value){
+    	Map<String, Object> result = (Map<String, Object>) cache.load("applyDictionaryRegionqu");
+        List<UserDictJson> tempList = (List<UserDictJson>)result.get(cityValue);
+        for(UserDictJson dict : tempList){
+    		if(value.equals(dict.getDict_value())){
+    			return dict.getDict_text();
+    		}
+    	}
+    	return "";    	
+    }
 }
