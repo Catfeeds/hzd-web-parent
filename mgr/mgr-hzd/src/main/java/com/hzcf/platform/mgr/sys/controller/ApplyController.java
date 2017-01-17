ce6016f8a54e055334e53b510b8c9d8f5ec07f6e
郝春贺 <Administrator@USER-20161207RW>
2017-01-17 15:21:41 +0800

    修改进件列表导出问题

--- mgr/mgr-hzd/src/main/java/com/hzcf/platform/mgr/sys/controller/ApplyController.java
+++ mgr/mgr-hzd/src/main/java/com/hzcf/platform/mgr/sys/controller/ApplyController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.hzcf.platform.common.util.log.Log;
 import com.hzcf.platform.core.user.model.UserApplyInfoVO;
 import com.hzcf.platform.core.user.model.UserVO;
+import com.hzcf.platform.core.user.service.DictUtilService;
 import com.hzcf.platform.mgr.sys.common.pageModel.DataGrid;
 import com.hzcf.platform.mgr.sys.common.pageModel.PageHelper;
 import com.hzcf.platform.mgr.sys.common.util.DateUtils;
@@ -34,6 +35,9 @@ public class ApplyController {
 	@Autowired
 	IApplyService applyService;
 	
+	@Autowired
+    DictUtilService dictUtilService;
+	
 	@RequestMapping(value = "/apply/list",method = RequestMethod.GET)
 	public String memberList() {
 	    return "apply/list";
@@ -53,7 +57,7 @@ public class ApplyController {
 	@RequestMapping(value = "/apply/excel",method = RequestMethod.POST)
     public void exportApplyUser(HttpServletResponse response, UserApplyInfoVO apply){
 		String title = "jinjian"+DateUtils.getDate();
-        String[] rowsName = new String[]{"序号","手机号","姓名","借款用途","借款用途详情","申请最低额度","申请最高额度","月还款最高额度","提交时间","进件状态"};
+        String[] rowsName = new String[]{"序号","手机号","姓名","身份证号","借款用途","借款用途详情","申请最低额度","申请最高额度","月还款最高额度","提交时间","进件状态"};
         List<UserApplyInfoVO> result = applyService.getUserApplyForSearch(apply);
         List<Object[]>  dataList = new ArrayList<Object[]>();
         Object[] objs = null;
@@ -61,25 +65,36 @@ public class ApplyController {
         for (int i = 0; i < result.size(); i++) {
         	vo = result.get(i);
             objs = new Object[rowsName.length];
-            objs[0] = i+1;
+            
+            String loanPurposeOne = vo.getLoanPurposeOne();
+            String loanPurposeOneValue = dictUtilService.convertLoanPurposeOne(loanPurposeOne);
+            String loanPurposeTwo = vo.getLoanPurposeTwo();
+            String loanPurposeTwoValue = dictUtilService.convertLoanPurposeTwo(loanPurposeOne,loanPurposeTwo);
+            
+           // objs[0] = i+1;
+            objs[0] = vo.getApplyId();
             objs[1] = vo.getMobile();
             objs[2] = vo.getName();
-            objs[3] = vo.getLoanPurposeOne();
-            objs[4] = vo.getLoanPurposeTwo();
-            objs[5] = vo.getMinApplyAmount();
-            objs[6] = vo.getMaxApplyAmount();
+            objs[3] = vo.getIdCard();
+            objs[4] = loanPurposeOneValue;   //vo.getLoanPurposeOne();
+            objs[5] = loanPurposeTwoValue;  //vo.getLoanPurposeTwo();
+            objs[6] = vo.getMinApplyAmount();
+            objs[7] = vo.getMaxApplyAmount();
             
-            objs[7] = vo.getMaxMonthlyPayment();
-            objs[8] = DateUtils.formatDate(vo.getApplySubmitTime(), "yyyy-MM-dd HH:mm:ss");
+            objs[8] = vo.getMaxMonthlyPayment();
+            objs[9] = DateUtils.formatDate(vo.getApplySubmitTime(), "yyyy-MM-dd HH:mm:ss");
             String status = vo.getStatus();
-            if(status.equals(ConstantsParam.USER_APPLYINFO_STATU_WJ)){
-            	objs[9] = "未进件";
+            if(status==null||status==""){
+            	objs[10] = "";
             }
-            if(status.equals(ConstantsParam.USER_APPLYINFO_STATU_JJ)){
-            	objs[9] = "已进件";
+            if((ConstantsParam.USER_APPLYINFO_STATU_WJ).equals(status)){
+            	objs[10] = "未进件";
             }
-            if(status.equals(ConstantsParam.USER_APPLYINFO_STATU_DSH)){
-            	objs[9] = "待审核";
+            if((ConstantsParam.USER_APPLYINFO_STATU_JJ).equals(status)){
+            	objs[10] = "已进件";
+            }
+            if((ConstantsParam.USER_APPLYINFO_STATU_DSH).equals(status)){
+            	objs[10] = "待审核";
             }
             dataList.add(objs);
         }
